55aa0038354db5ced0c9855d07e2512ce10209d5
Djxman01 <maximonieva1000@gmail.com>
2022-04-16 17:30:09 -0300

    cambios2

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,9 +2,35 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+   res.render('notes/new-notes');
+});
+
+router.post('/notes/new-notes', (req, res) => {
+  const {title, description} = req.body;
+  const errors = [];
+  if(!title) {
+    errors.push({text: 'Please Write a Title'});
+  }
+  if (!description) {
+    errors.push({text: 'Please Write a Description'});
+  }
+  if(errors.lenght > 0) {
+  res.render('notes/new-notes', {
+    errors,
+    title,
+    description
+  });
+  
+  } else {
+  res.send('ok');
+  }
+});
+
 router.get('/notes', (req, res) =>{
   res.send('Notes from database');
 
 });
 
 module.exports = router;
+
