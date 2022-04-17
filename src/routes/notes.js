4fa7f8e5460c78e8441853188b220f746e8f3fd2
Djxman01 <maximonieva1000@gmail.com>
2022-04-17 20:22:21 -0300

    cambios3

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
+
+
 
 router.get('/notes/add', (req, res) => {
    res.render('notes/new-notes');
 });
 
 router.post('/notes/new-notes', (req, res) => {
-  const {title, description} = req.body;
-  const errors = [];
-  if(!title) {
-    errors.push({text: 'Please Write a Title'});
-  }
-  if (!description) {
-    errors.push({text: 'Please Write a Description'});
-  }
-  if(errors.lenght > 0) {
-  res.render('notes/new-notes', {
-    errors,
-    title,
-    description
-  });
-  
-  } else {
-  res.send('ok');
-  }
+  const {title, description }= req.body;
+   const errors = [];
+   if(!title) {
+      errors.push({text: 'Please Write a Title'});
+   }
+   if(!description) {
+     errors.push({text: 'Please Write a Tescription'});
+   }
+   if(errors.length >= 0) {
+     res.render('notes/new-notes', {
+       errors,
+       title,
+       description 
+     });
+   } else {
+     
+   }
 });
+
 
 router.get('/notes', (req, res) =>{
   res.send('Notes from database');
