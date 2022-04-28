fc37f40d3c41882fd733ceac083618e8b41ce0dc
Djxman01 <maximonieva1000@gmail.com>
2022-04-28 16:42:26 -0300

    CRUD completo

--- src/index.js
+++ src/index.js
@@ -23,12 +23,13 @@ const app = express();
 
 
 // Settings
-app.set('port',process.env.PORT || 3001);
+app.set('port',process.env.PORT || 3000);
 app.set('views',path.join(__dirname,'views'))
 app.engine('.hbs', exphbs.engine({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
   partialsDir: path.join(app.get('views'), 'partials'),
+  notesDir: path.join(app.get('views'), 'notes'),
   extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -35,10 +35,26 @@ router.post('/notes/new-note', async (req, res) => {
 
 
 router.get('/notes', async (req, res) =>{
-  
 const notes = await Note.find().lean();
 res.render('notes/all-notes', {notes});  
-console.log(req.params.id);
+});
+
+router.get('/notes/edit/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id).lean();
+  res.render('notes/edit-note', {note});
+
+});
+
+router.put('/notes/edit-note/:id', async (req,res) =>{
+  const {title, description}=req.body;
+  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) =>{
+  await Note.findByIdAndDelete(req.params.id);
+  res.redirect('/notes');
+
 });
 
 
