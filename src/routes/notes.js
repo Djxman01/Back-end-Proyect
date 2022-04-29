664aa0231df3b245f5f61b08f4ac13b52764258d
Djxman01 <maximonieva1000@gmail.com>
2022-04-29 16:23:09 -0300

    finalfinal

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -5,12 +5,13 @@ const {body, validationResult} = require('express-validator');
 
 const Note = require('../models/Note');
 const req = require('express/lib/request');
+const {isAuthenticated} = require('../helpers/auth');
 
-router.get('/notes/add', (req, res) => {
+router.get('/notes/add', isAuthenticated,  (req, res) => {
    res.render('notes/new-note');
 });
 
-router.post('/notes/new-note', async (req, res) => {
+router.post('/notes/new-note',isAuthenticated, async (req, res) => {
   const {title, description }= req.body;
    const errors = [];
    if(!title) {
@@ -35,25 +36,25 @@ router.post('/notes/new-note', async (req, res) => {
 });
 
 
-router.get('/notes', async (req, res) =>{
+router.get('/notes',isAuthenticated, async (req, res) =>{
 const notes = await Note.find().lean();
 res.render('notes/all-notes', {notes});  
 });
 
-router.get('/notes/edit/:id', async (req, res) => {
+router.get('/notes/edit/:id', isAuthenticated, async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
   res.render('notes/edit-note', {note});
 
 });
 
-router.put('/notes/edit-note/:id', async (req,res) =>{
+router.put('/notes/edit-note/:id', isAuthenticated, async (req,res) =>{
   const {title, description}=req.body;
   await Note.findByIdAndUpdate(req.params.id, {title, description});
   req.flash('success_msg', 'Note Updated Successfully');
   res.redirect('/notes');
 });
 
-router.delete('/notes/delete/:id', async (req, res) =>{
+router.delete('/notes/delete/:id', isAuthenticated, async (req, res) =>{
   await Note.findByIdAndDelete(req.params.id);
   req.flash('success_msg', 'Note Deleted Successfully');
   res.redirect('/notes');
